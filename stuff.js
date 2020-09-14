0195792ec92fe18d4d0563db84246448814e2a5c
Colby Norman <colbynorm41@gmail.com>
2020-09-13 20:47:56 -0500

    tried setting up local storage

---
+++ stuff.js
@@ -0,0 +1,22 @@
+// var saveButton = $(".save-btn");
+
+// saveButton.on("click", function(event) {
+//     event.preventDefault();
+//     var alcoholType = $("#type").val();
+//     var alcoholName = $("#name").val();
+//     var alcoholRating = $("rating").val();
+
+//     localStorage.setItem('alcohol-type', alcoholType.toUpperCase());
+//     localStorage.setItem('alcohol-name', alcoholName.toUpperCase());
+//     localStorage.setItem('alcohol-rating', alcoholRating.toUpperCase());
+// });
+
+function getData() {
+    var alcoholType = document.getElementById("type").value;
+    var alcoholName = document.getElementById("name").value;
+    var alcoholRating = document.getElementById("rating").value;
+
+    localStorage.setItem("alcohol-type", alcoholType);
+    localStorage.setItem("alcohol-name", alcoholName);
+    localStorage.setItem("alcohol-rating", alcoholRating);
+}
